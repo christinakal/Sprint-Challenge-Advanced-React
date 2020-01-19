50ccd56ca9eb2dd1416413e0d1334557bd96c727
christina kaliora <develop@pirron.one>
2020-01-19 11:31:22 +0100

    Added material-ui and added card component into Player.js

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 const Player = (props) => {
+    const classes = useStyles();
+    const bull = <span className={classes.bullet}>•</span>;
+
     return (
-        <div>
-            <h2>{props.name}</h2>
-            <h3>{props.country}</h3>
-        </div>
+        <Card className={classes.card} variant="outlined">
+            <CardContent>
+                <Typography variant="h5" component="h2">
+                    {props.name}
+                </Typography>
+                <Typography variant="body2" component="p">
+                    {props.country}
+                </Typography>
+            </CardContent>
+        </Card>
     )
 }
 
